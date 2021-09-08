3e1093c0acfd12a695521635a85ce15d221264c4
johannesbyle <johannesbyle1@gmail.com>
2021-09-07 19:14:51 -0500

    finished bifurcation code

---
+++ Classical Mechanics I/bifurcation.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rcParams['text.usetex'] = True
+from math import pi, sin
+from tqdm import tqdm
+
+
+def f(x_, mu_=0.5):
+    return mu_ * sin(pi * x_)
+
+
+num_points = 1000
+mu_array = []
+x_array = []
+for mu in tqdm(np.linspace(0, 1, num_points * 10)):
+    x0 = 10 ** (-5)
+    x = [x0]
+    for _ in range(num_points):
+        x0 = f(x0, mu_=mu)
+        x.append(x0)
+    final_slice = int(num_points / 100)
+    mu_array += [mu] * final_slice
+    x_array += x[-final_slice:]
+plt.scatter(mu_array, x_array, c="blue", s=0.1)
+plt.ylabel("x")
+plt.xlabel(r"$\mu$")
+plt.savefig("bifurcation.svg")
+plt.show()
